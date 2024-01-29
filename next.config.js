b3d0014ea9c05c3452895b0cc94edfa460538bc7
Keenan Thompson <keenan.thompson67@gmail.com>
2024-01-30 01:12:40 +0800

    main: wip

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-})
+});
 
-module.exports = withNextra()
+const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+  trailingSlash: true,
+  output: 'export',
+};
+
+module.exports = {
+  ...withNextra(),
+  ...nextConfig,
+};
