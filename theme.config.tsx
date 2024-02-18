1288d39ba94973a12cfebc11878c867fab825560
Keenan Thompson <keenan.thompson67@gmail.com>
2024-02-18 22:20:00 +0800

    main: theme

--- theme.config.tsx
+++ theme.config.tsx
@@ -5,9 +5,10 @@ const config: DocsThemeConfig = {
   logo: <span>Ghost Remix</span>,
   head: (
     <>
+      <title>GhostRemix</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta property="og:title" content="GhostRemix Docs" />
-      <meta property="og:description" content="Documentation for GhostRemix" />
+      <meta property="og:title" content="GhostRemix" />
+      <meta property="og:description" content="GhostRemix Documentation" />
       <link rel="icon" href="/favicon.ico" />
     </>
   ),
