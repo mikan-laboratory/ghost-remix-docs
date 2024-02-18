4e447643e51ea6724f3b10de0b991eed9dd891a7
Keenan Thompson <keenan.thompson67@gmail.com>
2024-02-18 22:10:34 +0800

    main: head

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,6 +3,14 @@ import { DocsThemeConfig } from 'nextra-theme-docs';
 
 const config: DocsThemeConfig = {
   logo: <span>Ghost Remix</span>,
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="GhostRemix Docs" />
+      <meta property="og:description" content="Documentation for GhostRemix" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  ),
   project: {
     link: 'https://github.com/mikan-laboratory/ghost-remix',
   },
